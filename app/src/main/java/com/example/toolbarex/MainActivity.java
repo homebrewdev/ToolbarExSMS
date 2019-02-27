772b207eaa4b72421cff04cbd866c5c9c0b1d186
homebrewdev <xstall@yandex.ru>
2019-02-27 15:21:36 +0400

    Some corrections Некоторые косметические корректировки

--- app/src/main/java/com/example/toolbarex/MainActivity.java
+++ app/src/main/java/com/example/toolbarex/MainActivity.java
@@ -93,7 +93,6 @@ public class MainActivity extends AppCompatActivity
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
-        toggle.
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
